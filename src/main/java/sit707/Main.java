6f400ae2cea9f527a0d0544aec35b14557ac9d2a
Vishalk4kumar <s222342946@deakin.edu.au>
2024-03-14 12:57:23 +1100

    Seleniumtest Commit

---
+++ src/main/java/sit707/Main.java
@@ -0,0 +1,26 @@
+package sit707;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.setProperty(
+			"webdriver.chrome.driver",
+			"C:\\Users\\narwa\\Downloads\\chromedriver-win64\\chromedriver-win64/chromedriver.exe");
+			
+		WebDriver driver = new ChromeDriver();
+		System.out.println(driver);
+		
+		driver.get("https://www.google.com");
+		
+		try {
+			Thread.sleep(5*1000);
+		} catch (InterruptedException e) {
+			//TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.close();
+	}
+}
